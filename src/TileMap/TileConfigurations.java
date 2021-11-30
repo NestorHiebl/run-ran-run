ab68a0fd872a530d232987425de5b334af88ab19
Nestor Hiebl <bitbucket@nestorhiebl.com>
2021-11-30 18:30:16 +0100

    forgor file

--- src/TileMap/TileConfigurations.java
+++ src/TileMap/TileConfigurations.java
@@ -2,6 +2,8 @@ package TileMap;
 
 import Main.GamePanel;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.Vector;
 
 /**
@@ -15,9 +17,10 @@ public enum TileConfigurations {
     DEFAULT(),
     LEDGE();
 
-    private final int configLength;
+    private int configLength;
+    private final int configHeight;
     private final Vector<int[]> configuration;
-    private final boolean extendable;
+    private boolean extendable;
 
     public Vector<int[]> getConfiguration() {
         return this.configuration;
@@ -27,29 +30,32 @@ public enum TileConfigurations {
         return this.extendable;
     }
 
+    public int getLength() {
+        return this.configLength;
+    }
+
     TileConfigurations() {
-        configLength = (GamePanel.WIDTH / GamePanel.TILESIZE) * 2;
-        configuration = new Vector<>(configLength);
+        configHeight = GamePanel.HEIGHT / GamePanel.TILESIZE;
+
+        configuration = new Vector<>();
         switch (this) {
             case DEFAULT:
                 loadDefaultConfig();
-                this.extendable = true;
                 break;
             case LEDGE:
-                this.extendable = false;
+                loadLedgeConfig();
                 break;
             default:
                 /* This should never happen */
                 loadDefaultConfig();
-                this.extendable =false;
                 throw new IllegalStateException("Non-existent enum state passed to constructor");
         }
     }
 
     private void loadDefaultConfig() {
-        for (int i = 0; i < configLength; i++) {
-            this.configuration.addElement(new int[] {0, 0, 0, 0, 0, 0, 0, 21});
-        }
+        loadMapFile("Resources/Maps/tileConfigLedge.map");
+        this.configLength = configuration.size();
+        this.extendable = true;
     }
 
     private void loadLedgeConfig() {
@@ -77,6 +83,39 @@ public enum TileConfigurations {
         this.configuration.addElement(new int[] {0, 0, 0, 0, 0, 0, 0, 21});
         this.configuration.addElement(new int[] {0, 0, 0, 0, 0, 0, 0, 21});
         this.configuration.addElement(new int[] {0, 0, 0, 0, 0, 0, 0, 21});
+        this.extendable = false;
     }
 
+    private void loadMapFile(String s) {
+        try (BufferedReader br = new BufferedReader(new FileReader(s))) {
+
+            // Read map file
+            int numCols = Integer.parseInt(br.readLine()); // Line 1 holds the number of columns
+            int numRows = Integer.parseInt(br.readLine()); // Line 2 holds the number of rows
+
+            if (numRows != configHeight) throw new IllegalArgumentException("Invalid number of rows in map file");
+
+            /* Delimiter regex. Matches any whitespace character (\s) repeated any number of times (+) */
+            String delims = "\\s+";
+            for (int col = 0; col < numCols; col++) {
+                String line = br.readLine();
+
+                /* An array containing a single map column, the elements have to be converted to integers before being
+                * added to the config vector */
+                String[] tokens = line.split(delims);
+                if (tokens.length != configHeight) throw new IllegalArgumentException("Invalid number of rows in map file");
+
+                /* Build a valid integer array */
+                int[] column = new int[configHeight];
+                for (int i = 0; i < tokens.length; i++) {
+                    column[i] = Integer.parseInt(tokens[i]);
+                }
+
+                /* Add the integer array, representing the column, into the config vector */
+                this.configuration.addElement(column);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
